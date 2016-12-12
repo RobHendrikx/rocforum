7a0e9fdb21674d07fd4483e3429427959d6f46e8
Rob Hendrikx <robhendrikx94@hotmail.com>
2016-12-12 10:02:19 +0100

    push

--- pages/ict.php
+++ pages/ict.php
@@ -3,27 +3,26 @@
     <title>
         Projectforum
     </title>
-    <link rel="stylesheet" type="text/css" href="css/styles.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/styles.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
 </head>
 <body>
 <div id="navbar" class="container-fluid">
-    <img src="img/logo.png"/>
-    <a class="navtext" href="index.php">ROC Ter AA - Projectforum</a>
+    <img src="../img/logo.png"/>
+    <a class="navtext" href="../index.php">ROC Ter AA - Projectforum</a>
 </div>
-<div class="container">
-    <div class="page-header-margin-fix page-header page-heading">
-        <h1 class="pull-left">Colleges</h1>
+<div class="container" style="margin-top: 35px">
+    <div class="page-header page-heading">
+        <h1 class="pull-left">ICT</h1>
         <div class="clearfix"></div>
     </div>
-    <p class="lead">Kies hier welk college u de opdrachten voor wil zien.</p>
     <table class="table forum table-striped">
         <thead>
         <tr>
             <th class="cell-stat"></th>
             <th>
-                <h3>Colleges</h3>
+                <h3>Opdrachten</h3>
             </th>
             <th class="cell-stat text-center hidden-xs hidden-sm">Topics</th>
             <th class="cell-stat text-center hidden-xs hidden-sm">Posts</th>
@@ -34,7 +33,7 @@
         <tr>
             <td class="text-center"><i class="fa fa-question fa-2x text-primary"></i></td>
             <td>
-                <h4><a href="pages/ict.php">ICT College</a><br><small>Opdrachten voor de studenten van het ICT College</small></h4>
+                <h4><a href="#">ICT College</a><br><small>Opdrachten voor de studenten van het ICT College</small></h4>
             </td>
             <td class="text-center hidden-xs hidden-sm"><a href="#">9 542</a></td>
             <td class="text-center hidden-xs hidden-sm"><a href="#">89 897</a></td>
@@ -43,7 +42,7 @@
         <tr>
             <td class="text-center"><i class="fa fa-exclamation fa-2x text-danger"></i></td>
             <td>
-                <h4><a href="pages/business.php">Business College</a><br><small>Opdrachten voor de studenten van het Business College</small></h4>
+                <h4><a href="#">Business College</a><br><small>Opdrachten voor de studenten van het Business College</small></h4>
             </td>
             <td class="text-center hidden-xs hidden-sm"><a href="#">6532</a></td>
             <td class="text-center hidden-xs hidden-sm"><a href="#">152123</a></td>
@@ -52,7 +51,7 @@
         <tr>
             <td class="text-center"><i class="fa fa-exclamation fa-2x text-danger"></i></td>
             <td>
-                <h4><a href="pages/techniektechnologie.php">Techniek & Technologie College</a><br><small>Opdrachten voor de studenten van het Techniek en Technologie College</small></h4>
+                <h4><a href="#">Techniek & Technologie College</a><br><small>Opdrachten voor de studenten van het Techniek en Technologie College</small></h4>
             </td>
             <td class="text-center hidden-xs hidden-sm"><a href="#">6532</a></td>
             <td class="text-center hidden-xs hidden-sm"><a href="#">152123</a></td>
@@ -61,7 +60,7 @@
         <tr>
             <td class="text-center"><i class="fa fa-exclamation fa-2x text-danger"></i></td>
             <td>
-                <h4><a href="pages/bouwdesign.php">Bouw & Design College</a><br><small>Opdrachten voor de studenten van het Bouw & Design College</small></h4>
+                <h4><a href="#">Bouw & Design College</a><br><small>Opdrachten voor de studenten van het Bouw & Design College</small></h4>
             </td>
             <td class="text-center hidden-xs hidden-sm"><a href="#">6532</a></td>
             <td class="text-center hidden-xs hidden-sm"><a href="#">152123</a></td>
@@ -70,7 +69,7 @@
         <tr>
             <td class="text-center"><i class="fa fa-exclamation fa-2x text-danger"></i></td>
             <td>
-                <h4><a href="pages/dienstverlening.php">Dienstverlening College</a><br><small>Opdrachten voor de studenten van het Dienstverlening College</small></h4>
+                <h4><a href="#">Dienstverlening College</a><br><small>Opdrachten voor de studenten van het Dienstverlening College</small></h4>
             </td>
             <td class="text-center hidden-xs hidden-sm"><a href="#">6532</a></td>
             <td class="text-center hidden-xs hidden-sm"><a href="#">152123</a></td>
@@ -79,7 +78,7 @@
         <tr>
             <td class="text-center"><i class="fa fa-exclamation fa-2x text-danger"></i></td>
             <td>
-                <h4><a href="pages/onderwijskinderopvang.php">Onderwijs & Kinderopvang College</a><br><small>Opdrachten voor de studenten van het Bouw & Design College</small></h4>
+                <h4><a href="#">Onderwijs & Kinderopvang College</a><br><small>Opdrachten voor de studenten van het Bouw & Design College</small></h4>
             </td>
             <td class="text-center hidden-xs hidden-sm"><a href="#">6532</a></td>
             <td class="text-center hidden-xs hidden-sm"><a href="#">152123</a></td>
@@ -88,7 +87,7 @@
         <tr>
             <td class="text-center"><i class="fa fa-exclamation fa-2x text-danger"></i></td>
             <td>
-                <h4><a href="pages/zorgwelzijn.php">Zorg & Welzijn College</a><br><small>Opdrachten voor de studenten van het Zorg & Welzijn College</small></h4>
+                <h4><a href="#">Zorg & Welzijn College</a><br><small>Opdrachten voor de studenten van het Zorg & Welzijn College</small></h4>
             </td>
             <td class="text-center hidden-xs hidden-sm"><a href="#">6532</a></td>
             <td class="text-center hidden-xs hidden-sm"><a href="#">152123</a></td>
