e77f0cc464a49ff07693dc128f010af89e8a83a7
Mohamed Lahmar <mohamed.lahmar@outlook.com>
2017-01-26 13:27:52 +0100

    upload foto

--- pdf/test.php
+++ pdf/test.php
@@ -20,11 +20,14 @@ $y_axis = 55;
 $pdf->SetFont('Arial','B',15);
 
 // Logo laden.
-// $pdf->Image('`../img/logo_printen.jpg', 80, 10, 50, 0);
+ //$pdf->Image('`../img/logo_printen.jpg', 80, 10, 50, 0);
 
 // we gaan de resultaten ophalen uit database
+// Page header
 
-$sth = $conn->prepare("SELECT * FROM post WHERE idpost = '23' ");
+
+
+$sth = $conn->prepare("SELECT * FROM post WHERE idpost = '54' ");
 $sth->execute();
 
 /* Fetch all of the values of the first column */
@@ -32,40 +35,39 @@ $result = $sth->fetchAll(PDO::FETCH_ASSOC);
 // var_dump($result);
 
 
-// $result = $conn->query();
-
 
 foreach($result as $sss => $row) {
 
     //positie waar het feest van de Y-as moet beginnen
       $pdf->SetY($y_axis_initial);
     //posistie waar de X-as begind
-    $pdf->SetX(25);
-
-    //Titel
-    $pdf->Cell(25,6, 'Faxbericht',0 ,0);
+    $pdf->SetX(80);
 
     //Lettertype + letter grote
-    $pdf->SetFont('Arial','',12);
+    $pdf->SetFont('Arial','',30);    //Titel
+    $pdf->Cell(24,6, ' '.$row['projectnaam'],0 ,0);
 
+
+
+    //Lettertype + letter grote
+    $pdf->SetFont('Arial','',20);
     $pdf->SetY($y_axis);
     $pdf->SetX(25);
 
     //Aan wie willen we een fax sturen.
-    $pdf->Cell(1,25, 'Aan :',0 ,0);
-    $pdf->SetX(60);
+    $pdf->SetX(140);
 
     //Naam 1 wordt uit de database gehaald
-    $pdf->Cell(1,25,$row['projectnaam'],0,0,'L',2);
+    //$pdf->Cell(1,25,$row['projectnaam'],0,0,'L',2);
     $pdf->SetX(25);
 
     //Fax nummer
-    $pdf->Cell(1,40, 'Fax :',0 ,0);
-    $pdf->SetX(60);
+    $pdf->Cell(4,60, 'Aantal verwachte SBU  voor dit project: xx uur :',0 ,0);
+    $pdf->SetX(140);
 
     //Faxnummer wordt uit de database gehaald
     $pdf->Cell(1,40,$row['email_opdrachtgever'],0,0,'L',2); // kolomnaam
-    $pdf->SetX(25);
+    $pdf->SetX(254);
 
     $pdf->Cell(1,55, 'Van :',0 ,0);
     $pdf->SetX(60);
@@ -73,13 +75,15 @@ foreach($result as $sss => $row) {
 
 //Arial cursief 8
 
-$pdf->SetFont('Arial','', '8');
-$pdf->SetX(25);
+$pdf->SetFont('Arial','', '6');
+$pdf->SetX(4);
 $pdf->Cell(2,460,'  Voorwaarden:
 Dit project kan dienen als vervanging van een bestaande schoolopdracht. Dit is echter iets dat authentieker is dan een schoolopdracht.
-',0,0,'L',2);
-$pdf->SetX(35);
-$pdf->Cell(1,470,'Een schoolopdracht is voorwaardelijk voor de examinering en als het dus vervangen wordt voor een dergelijk alternatief is dus deze opdracht voorwaardelijk voor de examinering. Deze alternatieve opdracht zal dus ook met een voldoende moeten worden afgesloten.
+Een schoolopdracht is voorwaardelijk voor de examinering en als het dus vervangen wordt voor een dergelijk alternatief is dus deze opdracht voorwaardelijk voor de examinering.
+',0,0,'L',8);
+$pdf->SetX(4);
+$pdf->Cell(1,470,'
+ Deze alternatieve opdracht zal dus ook met een voldoende moeten worden afgesloten.
   Studiebelastingsuren (SBU):
 Onder de SBU wordt het aantal uren verstaan die nodig zijn voor de uitvoering van dit project. Het aantal uren is een inschatting door de docenten van het vak en is bespreekbaar',0,0,'L',2);
 
--- post.php
+++ post.php
@@ -13,6 +13,65 @@ $projectnaam = $omschrijving = $aantal_leden = $opleverdatum = $email_opdrachtge
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     include("mydatabase.php");
     if (!empty($_POST["projectnaam"] && !empty($_POST["omschrijving"]) && !empty($_POST["email_opdrachtgever"]))){
+
+      /* upload afbeelding */
+
+      $target_dir = "uploads/";
+      $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+      $uploadOk = 1;
+      $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+      // Check if image file is a actual image or fake image
+      if(isset($_POST["submit"])) {
+          $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+          if($check !== false) {
+              echo "File is an image - " . $check["mime"] . ".";
+              $uploadOk = 1;
+          } else {
+              echo "File is not an image.";
+              $uploadOk = 0;
+          }
+      }
+      // Check if file already exists
+      if (file_exists($target_file)) {
+          echo "Sorry, file already exists.";
+          $uploadOk = 0;
+      }
+      // Check file size
+      if ($_FILES["fileToUpload"]["size"] > 500000) {
+          echo "Sorry, your file is too large.";
+          $uploadOk = 0;
+      }
+      // Allow certain file formats
+      if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+      && $imageFileType != "gif" ) {
+          echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+          $uploadOk = 0;
+      }
+      // Check if $uploadOk is set to 0 by an error
+      if ($uploadOk == 0) {
+          echo "Sorry, your file was not uploaded.";
+      // if everything is ok, try to upload file
+      } else {
+          if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+              echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+              //SUCCESS
+
+              // var_dump($target_file);
+              // echo "<pre>"; var_dump($_FILES); echo "</pre>";
+              // die();
+
+
+
+          } else {
+              echo "Sorry, there was an error uploading your file.";
+          }
+      }
+
+
+
+      /* end upload afbeelding */
+
+
         $projectnaam = test_input($_POST["projectnaam"]);
         $omschrijving = test_input($_POST["omschrijving"]);
         $aantal_leden = test_input($_POST["aantal_leden"]);
@@ -28,8 +87,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         die();*/
 
         try {
-            $sql = "INSERT INTO post (projectnaam, omschrijving, aantal_leden, opleverdatum, email_opdrachtgever, telefoon_opdrachtgever, catid, userid)
-                        VALUES (:pnaam, :omschrijving, :aantal_leden, :opleverdatum, :email_opdrachtgever, :telefoon_opdrachtgever, :catid, :userid)";
+            $sql = "INSERT INTO post (projectnaam, omschrijving, aantal_leden, opleverdatum, email_opdrachtgever, telefoon_opdrachtgever, catid, userid, afbeeldingen)
+                        VALUES (:pnaam, :omschrijving, :aantal_leden, :opleverdatum, :email_opdrachtgever, :telefoon_opdrachtgever, :catid, :userid, :afbeeldingen)";
             $stmt = $conn->prepare($sql);
             $stmt->bindParam(':pnaam', $projectnaam);
             $stmt->bindParam(':omschrijving', $omschrijving);
@@ -37,6 +96,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $stmt->bindParam(':opleverdatum', $opleverdatum);
             $stmt->bindParam(':email_opdrachtgever', $email_opdrachtgever);
             $stmt->bindParam(':telefoon_opdrachtgever', $telefoon_opdrachtgever);
+            $stmt->bindParam(':afbeeldingen', $target_file);
             /*$stmt->bindParam(':categorie', $categorie);*/
             $stmt->bindParam(':catid', $catid);
             $stmt->bindParam(':userid', $_SESSION['user']['username']);
@@ -98,7 +158,7 @@ function test_input($data) {
         <a class="logintext" href="logout.php">Log uit</a>
     </span>
 </div>
-<form class="container" method="post" action="">
+<form class="container" method="post" action="" enctype="multipart/form-data">
         <div class="container" style="margin-top: 35px; padding-left: 0px; padding-right: 30px">
             <div class="page-header page-heading">
                 <h1 class="pull-left">Voeg een project toe</h1>
@@ -149,7 +209,8 @@ function test_input($data) {
         <div class="container" style="padding-left: 0px !important">
             <div class="col-md-6" style="padding-left: 0px !important">
                 <div class="post">
-                    <a href="#" type="button" class="btn btn-primary btn-lg btn-block">Upload afbeelding</a>
+                    <!-- <a href="#" type="button" class="btn btn-primary btn-lg btn-block">Upload afbeelding</a> -->
+                    <input class="btn btn-primary" type="file" name="fileToUpload" value="upload afbeelding" id="fileToUpload">
                 </div>
             </div>
             <div class="col-md-6">
